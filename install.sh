4944c71505146b49570582e51602960ffa904075
Pepijn de Geus (External) <pepijn.degeus@leaseplan.com>
2026-07-20 16:46:23 +0200

    neovim

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 which brew &>/dev/null && {
-	brew update
-	brew upgrade
-	brew install zsh maven git svn wget watch zopfli sqlite bat prettyping htop diff-so-fancy ncdu fzf jq fastfetch eza tmux nvim
+  brew update
+  brew upgrade
+  brew install zsh maven git svn wget watch zopfli sqlite bat prettyping htop diff-so-fancy ncdu fzf jq fastfetch eza tmux nvim
   brew install jandedobbeleer/oh-my-posh/oh-my-posh
   brew install lusingander/tap/serie
   brew install --cask wezterm
@@ -24,27 +24,26 @@ which apt-get &>/dev/null && {
 
   sudo add-apt-repository ppa:zhangsongcui3371/fastfetch
 
-	sudo apt-get update
-	sudo apt-get -y install zsh git tmux fzf fastfetch eza
-	cp /usr/share/doc/fzf/examples/key-bindings.zsh ~/.fzf.zsh
+  sudo apt-get update
+  sudo apt-get -y install zsh git tmux fzf fastfetch eza neovim
+  cp /usr/share/doc/fzf/examples/key-bindings.zsh ~/.fzf.zsh
   curl -s https://ohmyposh.dev/install.sh | bash -s -- -d ~/.bin
 }
 
 which pip &>/dev/null && {
-	pip install pygments
+  pip install pygments
 }
 
 # If this user's login shell is not already "zsh", attempt to switch.
 TEST_CURRENT_SHELL=$(expr "$SHELL" : '.*/\(.*\)')
 if [ "$TEST_CURRENT_SHELL" != "zsh" ]; then
-    # If this platform provides a "chsh" command (not Cygwin), do it, man!
-    if hash chsh >/dev/null 2>&1; then
-      printf "Time to change your default shell to zsh!\n"
-      chsh -s $(grep /zsh$ /etc/shells | tail -1)
-    # Else, suggest the user do so manually.
-    else
-      printf "I can't change your shell automatically because this system does not have chsh.\n"
-      printf "Please manually change your default shell to zsh!\n"
-    fi
+  # If this platform provides a "chsh" command (not Cygwin), do it, man!
+  if hash chsh >/dev/null 2>&1; then
+    printf "Time to change your default shell to zsh!\n"
+    chsh -s $(grep /zsh$ /etc/shells | tail -1)
+  # Else, suggest the user do so manually.
+  else
+    printf "I can't change your shell automatically because this system does not have chsh.\n"
+    printf "Please manually change your default shell to zsh!\n"
+  fi
 fi
-
